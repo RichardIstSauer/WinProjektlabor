2b66aec626391bc38389f4ea193f4687b5822e9e
Chub74 <43876399+Chub74@users.noreply.github.com>
2021-04-19 10:52:26 +0200

    Auslesen der iButtonID

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace WinProjektlabor
     public partial class Form1 : Form
     {
         UsbDetect usbDetect;
+        Drive drive = new Drive();
         string driveName;
         string driveLabel;
         public Form1()
@@ -22,28 +23,25 @@ namespace WinProjektlabor
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //usbDetect = new UsbDetect();
-            //usbDetect.DriveDetected += UsbDetect_DriveDetected;
-            //usbDetect.DriveRemoved += UsbDetect_DriveRemoved;
+            usbDetect = new UsbDetect();
+            usbDetect.DriveDetected += UsbDetect_DriveDetected;
+            usbDetect.DriveRemoved += UsbDetect_DriveRemoved;
         }
 
-       
 
-        //private void UsbDetect_DriveDetected(object sender, EventArgs e)
-        //{
-        //    driveName = ((DriveInfoEventArgs)e).DriveName;
-        //    driveLabel = ((DriveInfoEventArgs)e).DriveLabel;
-        //    //MessageBox.Show($"Drive {driveName} detected");
-        //    this.Invoke(new Action(() => lsbInfo.Items.Add(
-        //        $"Drive {driveName} '{driveLabel}' detected")));
-        //}
 
-        //private void UsbDetect_DriveRemoved(object sender, EventArgs e)
-        //{
-        //    driveName = ((DriveInfoEventArgs)e).DriveName;
-        //    //MessageBox.Show($"Drive {driveName} removed");
-        //    this.Invoke(new Action(() => lsbInfo.Items.Add(
-        //        $"Drive {this.driveName} removed")));
-        //}
+        private void UsbDetect_DriveDetected(object sender, EventArgs e)
+        {
+            driveName = ((DriveInfoEventArgs)e).DriveName;
+            driveLabel = ((DriveInfoEventArgs)e).DriveLabel;
+            //MessageBox.Show($"Drive {driveName} detected");
+            string iButtonID = drive.Start($"{driveName}");
+        }
+
+        private void UsbDetect_DriveRemoved(object sender, EventArgs e)
+        {
+            driveName = ((DriveInfoEventArgs)e).DriveName;
+            //MessageBox.Show($"Drive {driveName} removed");
+        }
     }
 }
