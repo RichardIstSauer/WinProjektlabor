7ea2185e9b079efffa6db28fbbc5d425e6851208
Chub74 <43876399+Chub74@users.noreply.github.com>
2021-04-19 10:40:18 +0200

    Auslesen der iButtonID vom USB Stick

---
+++ Drive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinProjektlabor
+{
+    class Drive
+    {
+        public string Start(string drive)
+        {
+            string iButtonID = "";
+            using (StreamReader sr = new StreamReader(drive + "\\config.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    iButtonID = sr.ReadLine();
+                }
+            }
+            return iButtonID;
+        }
+    }
+}
