f565aa65ba7e2f2466cc4e6d294f02ff575cea6f
Richard <richard.sauer@gmx.net>
2021-04-19 11:40:37 +0200

    Form1 in Login ausgelagert / Login USB Aktion erweitert

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WinProjektlabor
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -33,15 +33,15 @@ namespace WinProjektlabor
             this.lbl_StatusNachricht = new System.Windows.Forms.Label();
             this.lbl_Passwort = new System.Windows.Forms.Label();
             this.txtbx_Passwort = new System.Windows.Forms.TextBox();
+            this.btn_Anmelden = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_Status
             // 
             this.lbl_Status.AutoSize = true;
-            this.lbl_Status.Location = new System.Drawing.Point(50, 110);
-            this.lbl_Status.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Status.Location = new System.Drawing.Point(67, 135);
             this.lbl_Status.Name = "lbl_Status";
-            this.lbl_Status.Size = new System.Drawing.Size(65, 13);
+            this.lbl_Status.Size = new System.Drawing.Size(84, 17);
             this.lbl_Status.TabIndex = 0;
             this.lbl_Status.Text = "USB Status:";
             // 
@@ -49,20 +49,18 @@ namespace WinProjektlabor
             // 
             this.lbl_StatusNachricht.AutoSize = true;
             this.lbl_StatusNachricht.ForeColor = System.Drawing.Color.Red;
-            this.lbl_StatusNachricht.Location = new System.Drawing.Point(130, 110);
-            this.lbl_StatusNachricht.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_StatusNachricht.Location = new System.Drawing.Point(173, 135);
             this.lbl_StatusNachricht.Name = "lbl_StatusNachricht";
-            this.lbl_StatusNachricht.Size = new System.Drawing.Size(138, 13);
+            this.lbl_StatusNachricht.Size = new System.Drawing.Size(177, 17);
             this.lbl_StatusNachricht.TabIndex = 1;
             this.lbl_StatusNachricht.Text = "Bitte USB Stick einstecken!";
             // 
             // lbl_Passwort
             // 
             this.lbl_Passwort.AutoSize = true;
-            this.lbl_Passwort.Location = new System.Drawing.Point(52, 205);
-            this.lbl_Passwort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Passwort.Location = new System.Drawing.Point(69, 252);
             this.lbl_Passwort.Name = "lbl_Passwort";
-            this.lbl_Passwort.Size = new System.Drawing.Size(51, 17);
+            this.lbl_Passwort.Size = new System.Drawing.Size(60, 20);
             this.lbl_Passwort.TabIndex = 2;
             this.lbl_Passwort.Text = "Passwort";
             this.lbl_Passwort.UseCompatibleTextRendering = true;
@@ -70,25 +68,38 @@ namespace WinProjektlabor
             // 
             // txtbx_Passwort
             // 
-            this.txtbx_Passwort.Location = new System.Drawing.Point(112, 205);
-            this.txtbx_Passwort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtbx_Passwort.Location = new System.Drawing.Point(149, 252);
+            this.txtbx_Passwort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtbx_Passwort.Name = "txtbx_Passwort";
-            this.txtbx_Passwort.Size = new System.Drawing.Size(122, 20);
+            this.txtbx_Passwort.Size = new System.Drawing.Size(161, 22);
             this.txtbx_Passwort.TabIndex = 3;
             this.txtbx_Passwort.Visible = false;
             // 
-            // Form1
+            // btn_Anmelden
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btn_Anmelden.Location = new System.Drawing.Point(329, 252);
+            this.btn_Anmelden.Name = "btn_Anmelden";
+            this.btn_Anmelden.Size = new System.Drawing.Size(82, 23);
+            this.btn_Anmelden.TabIndex = 4;
+            this.btn_Anmelden.Text = "Anmelden";
+            this.btn_Anmelden.UseVisualStyleBackColor = true;
+            this.btn_Anmelden.Visible = false;
+            this.btn_Anmelden.Click += new System.EventHandler(this.btn_Anmelden_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 371);
+            this.ClientSize = new System.Drawing.Size(469, 457);
+            this.Controls.Add(this.btn_Anmelden);
             this.Controls.Add(this.txtbx_Passwort);
             this.Controls.Add(this.lbl_Passwort);
             this.Controls.Add(this.lbl_StatusNachricht);
             this.Controls.Add(this.lbl_Status);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +111,7 @@ namespace WinProjektlabor
         private System.Windows.Forms.Label lbl_StatusNachricht;
         private System.Windows.Forms.Label lbl_Passwort;
         private System.Windows.Forms.TextBox txtbx_Passwort;
+        private System.Windows.Forms.Button btn_Anmelden;
     }
 }
 
--- Login.cs
+++ Login.cs
@@ -10,18 +10,18 @@ using System.Windows.Forms;
 
 namespace WinProjektlabor
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
         UsbDetect usbDetect;
         Drive drive = new Drive();
         string driveName;
         string driveLabel;
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Login_Load(object sender, EventArgs e)
         {
             usbDetect = new UsbDetect();
             usbDetect.DriveDetected += UsbDetect_DriveDetected;
@@ -39,6 +39,9 @@ namespace WinProjektlabor
 
             this.Invoke(new Action(() => lbl_Passwort.Visible = true));
             this.Invoke(new Action(() => txtbx_Passwort.Visible = true));
+            this.Invoke(new Action(() => btn_Anmelden.Visible = true));
+            this.Invoke(new Action(() => lbl_StatusNachricht.Text = $"USB Stick {driveName} {driveLabel} wurde eingesteckt!"));
+            this.Invoke(new Action(() => lbl_StatusNachricht.ForeColor = Color.Green));
         }
 
         private void UsbDetect_DriveRemoved(object sender, EventArgs e)
@@ -47,6 +50,14 @@ namespace WinProjektlabor
             //MessageBox.Show($"Drive {driveName} removed");
             this.Invoke(new Action(() => lbl_Passwort.Visible = false));
             this.Invoke(new Action(() => txtbx_Passwort.Visible = false));
+            this.Invoke(new Action(() => btn_Anmelden.Visible = false));
+            this.Invoke(new Action(() => lbl_StatusNachricht.Text = "Bitte USB Stick einstecken!"));
+            this.Invoke(new Action(() => lbl_StatusNachricht.ForeColor = Color.Red));
+        }
+
+        private void btn_Anmelden_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
