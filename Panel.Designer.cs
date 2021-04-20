15a9a61ec446610de07e8c276b8d0064f69e05e6
Richard <richard.sauer@gmx.net>
2021-04-20 10:37:49 +0200

    TabControl für Verwaltung hinzugefügt

--- Panel.Designer.cs
+++ Panel.Designer.cs
@@ -29,89 +29,160 @@ namespace WinProjektlabor
         /// </summary>
         private void InitializeComponent()
         {
-            this.tc_Panel = new System.Windows.Forms.TabControl();
-            this.tp_Maschine = new System.Windows.Forms.TabPage();
-            this.pb_Maschine = new System.Windows.Forms.PictureBox();
-            this.lbl_Maschine = new System.Windows.Forms.Label();
             this.tp_Verwaltung = new System.Windows.Forms.TabPage();
-            this.tc_Panel.SuspendLayout();
+            this.tc_Verwaltung = new System.Windows.Forms.TabControl();
+            this.tp_USB = new System.Windows.Forms.TabPage();
+            this.tp_Member = new System.Windows.Forms.TabPage();
+            this.tp_Maschine = new System.Windows.Forms.TabPage();
+            this.lbl_Maschine = new System.Windows.Forms.Label();
+            this.pb_Maschine = new System.Windows.Forms.PictureBox();
+            this.tc_Panel = new System.Windows.Forms.TabControl();
+            this.tp_Log = new System.Windows.Forms.TabPage();
+            this.tp_Maschinen = new System.Windows.Forms.TabPage();
+            this.tp_Verwaltung.SuspendLayout();
+            this.tc_Verwaltung.SuspendLayout();
             this.tp_Maschine.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Maschine)).BeginInit();
+            this.tc_Panel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tp_Verwaltung
+            // 
+            this.tp_Verwaltung.Controls.Add(this.tc_Verwaltung);
+            this.tp_Verwaltung.Location = new System.Drawing.Point(4, 25);
+            this.tp_Verwaltung.Margin = new System.Windows.Forms.Padding(4);
+            this.tp_Verwaltung.Name = "tp_Verwaltung";
+            this.tp_Verwaltung.Padding = new System.Windows.Forms.Padding(4);
+            this.tp_Verwaltung.Size = new System.Drawing.Size(1059, 525);
+            this.tp_Verwaltung.TabIndex = 1;
+            this.tp_Verwaltung.Text = "Verwaltung";
+            this.tp_Verwaltung.UseVisualStyleBackColor = true;
+            // 
+            // tc_Verwaltung
+            // 
+            this.tc_Verwaltung.Controls.Add(this.tp_Member);
+            this.tc_Verwaltung.Controls.Add(this.tp_USB);
+            this.tc_Verwaltung.Controls.Add(this.tp_Maschinen);
+            this.tc_Verwaltung.Controls.Add(this.tp_Log);
+            this.tc_Verwaltung.Location = new System.Drawing.Point(0, 3);
+            this.tc_Verwaltung.Name = "tc_Verwaltung";
+            this.tc_Verwaltung.SelectedIndex = 0;
+            this.tc_Verwaltung.Size = new System.Drawing.Size(1063, 526);
+            this.tc_Verwaltung.TabIndex = 0;
+            // 
+            // tp_USB
+            // 
+            this.tp_USB.Location = new System.Drawing.Point(4, 25);
+            this.tp_USB.Name = "tp_USB";
+            this.tp_USB.Padding = new System.Windows.Forms.Padding(3);
+            this.tp_USB.Size = new System.Drawing.Size(1055, 497);
+            this.tp_USB.TabIndex = 1;
+            this.tp_USB.Text = "USB Sticks";
+            this.tp_USB.UseVisualStyleBackColor = true;
+            // 
+            // tp_Member
+            // 
+            this.tp_Member.Location = new System.Drawing.Point(4, 25);
+            this.tp_Member.Name = "tp_Member";
+            this.tp_Member.Padding = new System.Windows.Forms.Padding(3);
+            this.tp_Member.Size = new System.Drawing.Size(1055, 497);
+            this.tp_Member.TabIndex = 0;
+            this.tp_Member.Text = "Member";
+            this.tp_Member.UseVisualStyleBackColor = true;
+            // 
+            // tp_Maschine
+            // 
+            this.tp_Maschine.Controls.Add(this.pb_Maschine);
+            this.tp_Maschine.Controls.Add(this.lbl_Maschine);
+            this.tp_Maschine.Location = new System.Drawing.Point(4, 25);
+            this.tp_Maschine.Margin = new System.Windows.Forms.Padding(4);
+            this.tp_Maschine.Name = "tp_Maschine";
+            this.tp_Maschine.Padding = new System.Windows.Forms.Padding(4);
+            this.tp_Maschine.Size = new System.Drawing.Size(1059, 525);
+            this.tp_Maschine.TabIndex = 0;
+            this.tp_Maschine.Text = "Maschine";
+            this.tp_Maschine.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Maschine
+            // 
+            this.lbl_Maschine.AutoSize = true;
+            this.lbl_Maschine.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Maschine.Location = new System.Drawing.Point(11, 10);
+            this.lbl_Maschine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Maschine.Name = "lbl_Maschine";
+            this.lbl_Maschine.Size = new System.Drawing.Size(97, 24);
+            this.lbl_Maschine.TabIndex = 0;
+            this.lbl_Maschine.Text = "Maschine";
+            // 
+            // pb_Maschine
+            // 
+            this.pb_Maschine.Location = new System.Drawing.Point(15, 36);
+            this.pb_Maschine.Margin = new System.Windows.Forms.Padding(4);
+            this.pb_Maschine.Name = "pb_Maschine";
+            this.pb_Maschine.Size = new System.Drawing.Size(200, 185);
+            this.pb_Maschine.TabIndex = 1;
+            this.pb_Maschine.TabStop = false;
             // 
             // tc_Panel
             // 
             this.tc_Panel.Controls.Add(this.tp_Maschine);
             this.tc_Panel.Controls.Add(this.tp_Verwaltung);
             this.tc_Panel.Location = new System.Drawing.Point(0, 0);
+            this.tc_Panel.Margin = new System.Windows.Forms.Padding(4);
             this.tc_Panel.Name = "tc_Panel";
             this.tc_Panel.SelectedIndex = 0;
-            this.tc_Panel.Size = new System.Drawing.Size(800, 450);
+            this.tc_Panel.Size = new System.Drawing.Size(1067, 554);
             this.tc_Panel.TabIndex = 0;
             // 
-            // tp_Maschine
+            // tp_Log
             // 
-            this.tp_Maschine.Controls.Add(this.pb_Maschine);
-            this.tp_Maschine.Controls.Add(this.lbl_Maschine);
-            this.tp_Maschine.Location = new System.Drawing.Point(4, 22);
-            this.tp_Maschine.Name = "tp_Maschine";
-            this.tp_Maschine.Padding = new System.Windows.Forms.Padding(3);
-            this.tp_Maschine.Size = new System.Drawing.Size(792, 424);
-            this.tp_Maschine.TabIndex = 0;
-            this.tp_Maschine.Text = "Maschine";
-            this.tp_Maschine.UseVisualStyleBackColor = true;
+            this.tp_Log.Location = new System.Drawing.Point(4, 25);
+            this.tp_Log.Name = "tp_Log";
+            this.tp_Log.Size = new System.Drawing.Size(1055, 497);
+            this.tp_Log.TabIndex = 2;
+            this.tp_Log.Text = "Log";
+            this.tp_Log.UseVisualStyleBackColor = true;
             // 
-            // pb_Maschine
+            // tp_Maschinen
             // 
-            this.pb_Maschine.Location = new System.Drawing.Point(11, 29);
-            this.pb_Maschine.Name = "pb_Maschine";
-            this.pb_Maschine.Size = new System.Drawing.Size(150, 150);
-            this.pb_Maschine.TabIndex = 1;
-            this.pb_Maschine.TabStop = false;
-            // 
-            // lbl_Maschine
-            // 
-            this.lbl_Maschine.AutoSize = true;
-            this.lbl_Maschine.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Maschine.Location = new System.Drawing.Point(8, 8);
-            this.lbl_Maschine.Name = "lbl_Maschine";
-            this.lbl_Maschine.Size = new System.Drawing.Size(77, 18);
-            this.lbl_Maschine.TabIndex = 0;
-            this.lbl_Maschine.Text = "Maschine";
-            // 
-            // tp_Verwaltung
-            // 
-            this.tp_Verwaltung.Location = new System.Drawing.Point(4, 22);
-            this.tp_Verwaltung.Name = "tp_Verwaltung";
-            this.tp_Verwaltung.Padding = new System.Windows.Forms.Padding(3);
-            this.tp_Verwaltung.Size = new System.Drawing.Size(792, 424);
-            this.tp_Verwaltung.TabIndex = 1;
-            this.tp_Verwaltung.Text = "Verwaltung";
-            this.tp_Verwaltung.UseVisualStyleBackColor = true;
+            this.tp_Maschinen.Location = new System.Drawing.Point(4, 25);
+            this.tp_Maschinen.Name = "tp_Maschinen";
+            this.tp_Maschinen.Size = new System.Drawing.Size(1055, 497);
+            this.tp_Maschinen.TabIndex = 3;
+            this.tp_Maschinen.Text = "Maschinen";
+            this.tp_Maschinen.UseVisualStyleBackColor = true;
             // 
             // Panel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.tc_Panel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Panel";
             this.Text = "Panel";
             this.Load += new System.EventHandler(this.Panel_Load);
-            this.tc_Panel.ResumeLayout(false);
+            this.tp_Verwaltung.ResumeLayout(false);
+            this.tc_Verwaltung.ResumeLayout(false);
             this.tp_Maschine.ResumeLayout(false);
             this.tp_Maschine.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Maschine)).EndInit();
+            this.tc_Panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tc_Panel;
-        private System.Windows.Forms.TabPage tp_Maschine;
         private System.Windows.Forms.TabPage tp_Verwaltung;
+        private System.Windows.Forms.TabControl tc_Verwaltung;
+        private System.Windows.Forms.TabPage tp_Member;
+        private System.Windows.Forms.TabPage tp_USB;
+        private System.Windows.Forms.TabPage tp_Maschinen;
+        private System.Windows.Forms.TabPage tp_Log;
+        private System.Windows.Forms.TabPage tp_Maschine;
         private System.Windows.Forms.PictureBox pb_Maschine;
         private System.Windows.Forms.Label lbl_Maschine;
+        private System.Windows.Forms.TabControl tc_Panel;
     }
 }
